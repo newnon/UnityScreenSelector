645a15da4ca8642ed3723e4e10ac9d887ad9e800
s_perepelitsa <s.perepelitsa@spherelab.ru>
2020-02-17 15:04:12 +0300

    c++ project

---
+++ ScreenSelector/ScreenSelector.cpp
@@ -0,0 +1,25 @@
+﻿// ScreenSelector.cpp : Определяет точку входа для приложения.
+//
+
+#include "stdafx.h"
+#include "ScreenSelector.h"
+#include <string>
+
+bool DisplayScreenSelector(std::wstring& commandline);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+	std::wstring line;
+
+	DisplayScreenSelector(line);
+
+    return 0;
+}
